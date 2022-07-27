d1109a4d89998e6346d2aec7652603ea7208a68d
mnseong <mskweon@icloud.com>
2022-07-27 15:23:42 +0900

    login, register 간 이동 구현

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -3,6 +3,9 @@ import AuthWrapper from '../components/auth/AuthWrapper';
 import AuthContent from '../components/auth/AuthContent';
 import InputWithLabel from './../components/InputWithLabel';
 import AuthButton from './../components/auth/AuthButton';
+import Combobox from './../components/Combobox';
+import RightAlignedLink from './../components/RightAlignedLink';
+import { bankList } from "./../data/bankList";
 import { Route, Routes, Link, Outlet } from 'react-router-dom';
 import { useCombobox } from "downshift";
 
@@ -19,6 +22,7 @@ function Register() {
                         <InputWithLabel label="비밀번호" name="password" placeholder="  비밀번호" type="password"/>
                         <InputWithLabel label="" name="passwordConfirm" placeholder="  비밀번호 확인" type="password"/>
                         <AuthButton onClick={setFlag}>다음</AuthButton>
+                        <RightAlignedLink to="/login">로그인</RightAlignedLink>
                     </AuthContent>
                 </AuthWrapper>
             );
@@ -30,6 +34,7 @@ function Register() {
             return (
                 <AuthWrapper>
                     <AuthContent title="선정산 받으실 계좌를 입력해주세요">
+                        <Combobox label="" placeholder="  은행" items={ bankList } />
                         <InputWithLabel label="" name="account" placeholder="  계좌번호"/>
                     </AuthContent>
                     <AuthButton>회원가입 완료</AuthButton>
