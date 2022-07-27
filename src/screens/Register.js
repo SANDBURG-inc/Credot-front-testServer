757919f0829e8add235f0f94357787f9c018a68e
mnseong <mskweon@icloud.com>
2022-07-27 14:27:45 +0900

    CustomerInfo 구현

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import AuthWrapper from '../components/auth/AuthWrapper';
 import AuthContent from '../components/auth/AuthContent';
 import InputWithLabel from './../components/InputWithLabel';
 import AuthButton from './../components/auth/AuthButton';
+import { Route, Routes, Link, Outlet } from 'react-router-dom';
+import { useCombobox } from "downshift";
 
-
-class Register extends Component {
-    render() {
-        return (
-            <AuthWrapper>
-                <AuthContent title="회원가입">
-                    <InputWithLabel label="이름" name="name" placeholder="  이름"/>
-                    <InputWithLabel label="이메일" name="email" placeholder="  이메일"/>
-                    <InputWithLabel label="연락처" name="phoneNum" placeholder="  연락처"/>
-                    <InputWithLabel label="비밀번호" name="password" placeholder="  비밀번호" type="password"/>
-                    <InputWithLabel label="" name="passwordConfirm" placeholder="  비밀번호 확인" type="password"/>
-                    <AuthButton>회원가입</AuthButton>
-                </AuthContent>
-            </AuthWrapper>
-        );
+function Register() {
+    let [flag, setFlag] = useState(0);
+    class CustomerInfo extends Component {
+        render() {
+            return (
+                <AuthWrapper>
+                    <AuthContent title="회원가입">
+                        <InputWithLabel label="이름" name="name" placeholder="  이름"/>
+                        <InputWithLabel label="이메일" name="email" placeholder="  이메일"/>
+                        <InputWithLabel label="연락처" name="phoneNum" placeholder="  연락처"/>
+                        <InputWithLabel label="비밀번호" name="password" placeholder="  비밀번호" type="password"/>
+                        <InputWithLabel label="" name="passwordConfirm" placeholder="  비밀번호 확인" type="password"/>
+                        <AuthButton onClick={setFlag}>다음</AuthButton>
+                    </AuthContent>
+                </AuthWrapper>
+            );
+        }
     }
+
+    return flag === 0
+           ? <CustomerInfo></CustomerInfo>
+           : <div></div>
 }
 
 export default Register;
